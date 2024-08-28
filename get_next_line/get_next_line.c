987acecd49319666c98be28c2819ac2ca18175a4
AnglGG <angelgallego999@gmail.com>
2024-08-28 12:52:53 +0200

    Get_next_line mandatory

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: anggalle <anggalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 00:14:38 by anggalle          #+#    #+#             */
-/*   Updated: 2024/07/26 18:23:03 by anggalle         ###   ########.fr       */
+/*   Updated: 2024/08/21 15:34:00 by anggalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-static char	*ft_strchr(char *s, int c)
+static char	*ft_find_char(char *s, int c)
 {
 	unsigned int	i;
 	char			cc;
@@ -30,77 +31,77 @@ static char	*ft_strchr(char *s, int c)
 	return (NULL);
 }
 
-static char	*fill_line_buffer(int fd, char *left_c, char *buffer)
+static char	*read_and_append(int fd, char *remaining, char *buffer)
 {
-	ssize_t	reader;
-	char	*tmp;
+	ssize_t	bytes_read;
+	char	*temp;
 
-	reader = 1;
-	while (reader > 0)
+	bytes_read = 1;
+	while (bytes_read > 0)
 	{
-		reader = read(fd, buffer, BUFFER_SIZE);
-		if (reader == -1)
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
 		{
-			free(left_c);
+			free(remaining);
 			return (NULL);
 		}
-		else if (reader == 0)
+		else if (bytes_read == 0)
 			break ;
-		buffer[reader] = 0;
-		if (!left_c)
-			left_c = ft_strdup("");
-		tmp = left_c;
-		left_c = ft_strjoin(tmp, buffer);
-		free(tmp);
-		tmp = NULL;
-		if (ft_strchr(buffer, '\n'))
+		buffer[bytes_read] = '\0';
+		if (!remaining)
+			remaining = ft_strdup("");
+		temp = remaining;
+		remaining = ft_strjoin(temp, buffer);
+		free(temp);
+		if (ft_find_char(buffer, '\n'))
 			break ;
 	}
-	return (left_c);
+	return (remaining);
 }
 
-static char	*set_left(char	*line_buffer)
+static char	*extract_line(char	*line)
 {
-	char	*left_c;
+	char	*remaining;
 	int		i;
 
 	i = 0;
-	while (line_buffer[i] != '\0' && line_buffer[i] != '\n')
+	while (line[i] != '\0' && line[i] != '\n')
 		i ++;
-	if (line_buffer[i] == 0 || line_buffer[1] == 0)
+	if (line[i] == '\0')
 		return (NULL);
-	left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
-	if (*left_c == 0)
+	remaining = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (*remaining == '\0')
 	{
-		free(left_c);
-		left_c = NULL;
+		free(remaining);
+		remaining = NULL;
 	}
-	line_buffer[i + 1] = 0;
-	return (left_c);
+	line[i + 1] = '\0';
+	return (remaining);
 }
 
 char	*get_next_line(int fd)
 {
 	char		*buffer;
 	char		*line;
-	static char	*left_c;
+	static char	*remaining_content;
 
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || !buffer)
 	{
 		free(buffer);
-		free(left_c);
-		left_c = NULL;
-		buffer = NULL;
 		return (NULL);
 	}
-	if (!buffer)
+	if (read(fd, 0, 0) < 0)
+	{
+		free(buffer);
+		free(remaining_content);
+		remaining_content = NULL;
 		return (NULL);
-	line = fill_line_buffer(fd, left_c, buffer);
+	}
+	line = read_and_append(fd, remaining_content, buffer);
 	free(buffer);
-	buffer = NULL;
 	if (!line)
 		return (NULL);
-	left_c = set_left(line);
+	remaining_content = extract_line(line);
 	return (line);
 }
