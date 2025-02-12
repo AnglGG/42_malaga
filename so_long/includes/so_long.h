c6d14e9f99864ae524b933abb05518b76d94e092
Angel <angelgallego999@gmail.com>
2025-02-12 14:12:38 +0100

    Corregida parte map + norminette de esa parte

--- so_long/includes/so_long.h
+++ so_long/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: anggalle <anggalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 18:00:38 by anggalle          #+#    #+#             */
-/*   Updated: 2025/02/12 12:38:30 by anggalle         ###   ########.fr       */
+/*   Updated: 2025/02/12 14:12:20 by anggalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 # define LEFT    97
 # define RIGHT   100
 
-/* ==================== */
+/* ====================== */
 /* Inclusiones de sistema */
-/* ==================== */
+/* ====================== */
+
 # include <stdlib.h>      // malloc, free, exit, EXIT_FAILURE
 # include <unistd.h>      // close, read, etc.
 # include <fcntl.h>       // open, O_RDONLY, etc.
@@ -27,80 +28,67 @@
 # include <stddef.h>      // NULL
 # include <X11/keysym.h>  // Key symbols
 
-/* ==================== */
+/* =============================== */
 /* Inclusiones de MiniLibX y libft */
-/* ==================== */
+/* =============================== */
 # include "../minilibx-linux/mlx.h"  // Ajusta la ruta según tu estructura
 # include "../libft/libft.h"         // Para ft_printf, get_next_line, etc.
 
 /* ==================== */
-/* Estructuras y declaraciones para renderizado */
+/* Estructuras			*/
 /* ==================== */
+
+typedef struct s_game
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	t_map		map;
+	t_images	imgs;
+}	t_game;
+
+typedef struct s_map
+{
+	char	**copy_map;
+	int		player_x;
+	int		player_y;
+	int		move_count;
+	int		num_lines;
+	int		num_columns;
+	int		e;
+	int		c;
+	int		p;
+	int		f;
+	int		w;
+}	t_map;
 
 typedef struct s_images
 {
-    void    *wall;
-    void    *floor;
-    void    *player;
-    void    *exit;
-    void    *collectible;
-    int     width;
-    int     height;
-}   t_images;
-
-/* Prototipos de funciones de renderizado */
-void    load_images(void *mlx_ptr, t_images *imgs);
-void    render_map(void *mlx_ptr, void *win_ptr, char **map, t_images *imgs);
+	void	*wall;
+	void	*floor;
+	void	*player;
+	void	*exit;
+	void	*collectible;
+	int		width;
+	int		height;
+}	t_images;
 
 /* ==================== */
-/* Funciones de manejo del mapa */
+/* map.c				*/
 /* ==================== */
 
-/* Imprime un mensaje de error y finaliza el programa */
-void    error_and_exit(const char *msg);
-
-/* Cuenta el número de líneas del archivo de mapa */
-int     count_lines(const char *filename);
-
-/* Lee el mapa desde el archivo y devuelve un array de cadenas.
-   El número de líneas se guarda en *num_lines */
-char    **read_map(const char *filename, int *num_lines);
-
-/* Verifica si el mapa es rectangular.
-   Devuelve 1 si es rectangular, 0 en caso contrario */
-int     check_rectangular(char **map);
-
-/* Recorre el mapa y cuenta los caracteres especiales.
-   Los contadores se guardan en el array 'characters' (índices:
-   0: '0', 1: '1', 2: 'E', 3: 'C', 4: 'P') */
-void    validate_characters(char **map, int *characters);
-
-/* Comprueba que el mapa esté cerrado por paredes */
-void    check_boundaries(char **map, int num_lines);
-
-/* Realiza todas las validaciones del mapa:
-   - Rectangularidad
-   - Carácter válidos y cantidad mínima/máxima de elementos
-   - Límites cerrados por paredes */
-void    validate_map(char **map, int num_lines);
+void	inicialize_map(t_map *map, char *filename);
+char	**read_map(char *filename, int *num_lines);
+void	count_cols(t_map *map);
+int		count_lines(const char *filename);
 
 /* ==================== */
-/* Estructura y funciones para eventos y datos del juego */
+/* parse_map.c			*/
 /* ==================== */
 
-/* Estructura que contiene todos los datos del juego */
-typedef struct s_game
-{
-    void    *mlx_ptr;
-    void    *win_ptr;
-    char    **map;
-    t_images imgs;
-    int     player_x;
-    int     player_y;
-    int     move_count;
-}   t_game;
-
-/* Prototipo de la función que manejará los eventos de teclado */
-int     handle_key(int keysym, t_game *game);
+void	validate_map(t_map *map);
+void	validate_characters(t_map *map);
+void	check_boundaries(t_map map);
+void	check_characters(t_map *map);
+void	check_rectangular(char **map);
 
 #endif
